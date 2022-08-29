2e045825161249f7c7c8d9b4a1b5ea9526b7f64d
langford <langfordquarshie21@gmail.com>
2022-08-29 23:29:23 +0000

    fix: drawer size and spacin

--- components/Drawer/styles.js
+++ components/Drawer/styles.js
@@ -1,7 +1,7 @@
 const styles = {
   drawerWrapper: `fixed top-0 left-0 w-screen h-screen bg-[#000]`,
   drawer: `p-10 text-center`,
-  link:`text-3xl font-meduim mb-10 font-medium hover:text-brand transition`,
+  link:`text-2xl font-meduim mb-6 font-medium hover:text-brand transition`,
   close:`text-7xl mb-10 block`,
 };
 
