4b9b57207e787306cc924e1dad314846f06d528b
langford <langfordquarshie21@gmail.com>
2022-08-30 00:39:31 +0000

    react-scroll-parallax + react-reveal

--- components/Flex/index.js
+++ components/Flex/index.js
@@ -1,4 +1,3 @@
-// import { isMobile } from "react-device-detect";
 import { useParallax } from "react-scroll-parallax";
 import Image from "next/image";
 import img1 from "../../assets/1.png";
@@ -8,7 +7,6 @@ import styles from "./styles";
 const Flex = (props) => {
   const { reverse, img, title, content, subtitle, imgAlt } = props;
   const { ref } = useParallax({ speed: 30 });
-  // const { hRef } = useParallax({ speed: 40, translateX: ["-100px", "30px"] });
 
   if (!reverse)
     return (
@@ -16,8 +14,7 @@ const Flex = (props) => {
         <section>
           <div className="lg:flex items-center justify-between">
             <div className="flex-1">
-              {/* <Parallax translateX={["-100px", "30px"]}> */}
-              <div className="lg:p-20 p-6 py-20">
+              <div className="lg:p-20 p-6 py-20" ref={ref}>
                 <small className={styles.subtitle}>{subtitle}</small>
                 <p className="lg:text-7xl text-4xl font-bold afterline">
                   {title}
@@ -25,9 +22,8 @@ const Flex = (props) => {
                 <p className="mt-5 text-xl font-medium leading-8">{content}</p>
                 <StartLink />
               </div>
-              {/* </Parallax> */}
             </div>
-            <div className="flex-1" ref={ref}>
+            <div className="flex-1">
               <Image src={img1} alt={imgAlt} />
             </div>
           </div>
@@ -39,11 +35,11 @@ const Flex = (props) => {
     <>
       <section>
         <div className="flex lg:flex-row flex-col-reverse items-center justify-between">
-          <div className="flex-1" ref={ref}>
+          <div className="flex-1">
             <Image src={img1} alt={imgAlt} />
           </div>
           <div className="flex-1">
-            <div className="lg:p-20 p-6 py-20">
+            <div className="lg:p-20 p-6 py-20" ref={ref}>
               <small className={styles.subtitle}>{subtitle}</small>
               <p className="lg:text-7xl text-4xl font-bold afterline">
                 {title}
