b5d3391e73ab30c455b1ca23b2476a8817527635
langford <langfordquarshie21@gmail.com>
2022-09-03 16:51:26 +0000

    added saucy grills

--- components/Projects/index.js
+++ components/Projects/index.js
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import styles from "./styles";
 
 const Projects = () => {
@@ -5,7 +6,7 @@ const Projects = () => {
     <>
       <section>
         <ul className="grid-wrapper lg:columns-3 sm:columns-2">
-          <li className="lg:h-[400px] h-[200px] flex justify-center flex-col p-[20px] lg:p-[30px] bg-[#f1f1f111]">
+          <li className="lg:h-[500px] h-[200px] flex justify-center flex-col p-[20px] lg:p-[30px] bg-[#f1f1f111]">
             <h2 className="lg:text-5xl text-4xl font-bold afterline">
               Our Works
             </h2>
@@ -15,23 +16,30 @@ const Projects = () => {
           </li>
           <li>
             <div id="grid_4" className="grid-item"></div>
-            <div className={styles.gridText}>sample #1</div>
+            {/* <div className={styles.gridText}>sample #1</div> */}
           </li>
           <li>
             <div id="grid_3" className="grid-item"></div>
-            <div className={styles.gridText}>sample #1</div>
+            {/* <div className={styles.gridText}>sample #1</div> */}
           </li>
           <li>
             <div id="grid_1" className="grid-item"></div>
-            <div className={styles.gridText}>sample #1</div>
+            {/* <div className={styles.gridText}>sample #1</div> */}
           </li>
           <li>
-            <div id="grid_5" className="grid-item"></div>
-            <div className={styles.gridText}>sample #1</div>
+            <Link
+              passHref
+              href="https://vapourwarestudiosprojs.on.drv.tw/SaucyGrills/"
+            >
+              <a target="_blank" rel="noopener noreferrer">
+                <div id="grid_5" className="grid-item"></div>
+                <div className={styles.gridText}>Saucy Grills</div>
+              </a>
+            </Link>
           </li>
           <li>
             <div id="grid_2" className="grid-item"></div>
-            <div className={styles.gridText}>sample #1</div>
+            {/* <div className={styles.gridText}>sample #1</div> */}
           </li>
         </ul>
       </section>
